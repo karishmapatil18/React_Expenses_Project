6b9dc6982146aacec1226e8b1e997ffadc642c29
karishmapatil18 <karishmapatil185@gmail.com>
2024-01-15 22:52:30 +0530

    React First Project Of Expenses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Expenses from './components/Expenses';
+
+function App(){
+    let expenses=[
+        {
+            id: 'e1',
+            title: 'School Fee',
+            amount: 250,
+            date: new Date(2021,5,23)
+
+        },
+        {
+            id: 'e2',
+            title: 'Books',
+            amount: 650,
+            date: new Date(2023,6,23)
+
+        },
+        {
+            id: 'e3',
+            title: 'House Rent',
+            amount: 100,
+            date: new Date(2023,7,4)
+
+        },
+        {
+            id: 'e4',
+            title: 'Food',
+            amount: 745,
+            date: new Date(2023,4,14)
+
+        }
+    ];
+
+    return (
+        <div>
+            <h2>Lets Get Started</h2>
+            <Expenses item = {expenses}/>
+                 
+        </div>
+       
+    );
 }
 
 export default App;
